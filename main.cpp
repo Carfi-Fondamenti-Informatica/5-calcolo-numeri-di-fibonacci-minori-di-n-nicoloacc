1213389923c31bc79bb9f47164a86d216331b121
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-04-05 09:26:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+    int n=0, risultato=0, a=0, b=1;
+    cout << "inserire un valore massimo n" << endl;
+    cin >> n;
+    for (int i = 1 ; i<n; i++)
+
+    if (a+b<=n) {
+        {
+            risultato = a + b;
+            b = a;
+            a = risultato;
+            cout << risultato << endl;
+        }
+    }
    return 0;
 }
